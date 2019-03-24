35c16bdbe04084684239ef57f3237c049efb4ebb
Najdes <krystian.najdecki@gmail.com>
2019-03-24 23:50:43 +0100

    Zmieniona funkcjonalność

---
+++ app/src/main/java/com/example/projektlodowka/produkty_activity.java
@@ -0,0 +1,13 @@
+package com.example.projektlodowka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class produkty_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produkty_activity);
+    }
+}
