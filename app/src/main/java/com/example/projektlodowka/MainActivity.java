63627c25c24497edd651c1e577fab47d8057daa3
Najdes <krystian.najdecki@gmail.com>
2019-03-25 15:26:56 +0100

    Lekko poprawiona funkcjonalność; -dodany toolbar na wszystkich activity, -dodany menu buttton, -zmiana ikoy i nazwy aplikacji -jedyny problem to "przykryty" textView przez toolbar

--- app/src/main/java/com/example/projektlodowka/MainActivity.java
+++ app/src/main/java/com/example/projektlodowka/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.projektlodowka;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +13,6 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
-    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,20 +22,27 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
+
+
                 new NavigationView.OnNavigationItemSelectedListener() {
+                    private MenuItem menuItem;
+
+
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        this.menuItem = menuItem;
                         // set item as selected to persist highlight
                         menuItem.setChecked(true);
                         int id = menuItem.getItemId();
-                        // close drawer when item is tapped
+
                         drawerLayout.closeDrawers();
                         switch(id){
                             case R.id.nav_produkty:
@@ -55,11 +62,26 @@ public class MainActivity extends AppCompatActivity {
                                 startActivity(intent3);
                                 break;
                         }
-
+                        drawerLayout.closeDrawers();
 
                         return true;
                     }
                 });
 
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
