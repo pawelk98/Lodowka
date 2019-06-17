c11d5c9716bf52c93b7c122274abc61b5b416994
Juzernejm <dawidpy@gmail.com>
2019-06-17 15:55:31 +0200

    Poprawa wyglądu UI

--- app/src/main/java/com/example/projektlodowka/RecipeEditFragment.java
+++ app/src/main/java/com/example/projektlodowka/RecipeEditFragment.java
@@ -276,6 +276,8 @@ public class RecipeEditFragment extends Fragment {
         });
 
     }
+
+
     private void openGallery() {
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
