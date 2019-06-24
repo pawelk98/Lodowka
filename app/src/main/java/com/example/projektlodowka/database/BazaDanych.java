798ba5973223577cda1db9a6d142b85bd01c8328
Juzernejm <dawidpy@gmail.com>
2019-06-25 01:01:37 +0200

    bugfix w bazie danych

--- app/src/main/java/com/example/projektlodowka/database/BazaDanych.java
+++ app/src/main/java/com/example/projektlodowka/database/BazaDanych.java
@@ -86,6 +86,13 @@ public abstract class BazaDanych extends RoomDatabase {
             przepisDao.insert(new Przepis(nazwa, czas, opis, poraDnia));
         }
 
+        private void insertPrzepis(String nazwa, int czas, String opis, int poraDnia, Drawable obrazek)
+        {
+            Przepis p = new Przepis(nazwa,czas, opis, poraDnia);
+            p.setImage(getBytesFromBitmap(obrazek));
+            przepisDao.insert(p);
+        }
+
         private void insertProdukt(String nazwa, int typ, int ilosc, Drawable obrazek){
             Produkt p = new Produkt(nazwa, typ, ilosc);
             p.setImage(getBytesFromBitmap(obrazek));
@@ -129,67 +136,78 @@ public abstract class BazaDanych extends RoomDatabase {
                 insertProdukt("kefir", 1, 0, c.getResources().getDrawable(R.drawable.kefir));
                 insertProdukt("przecier pomidorowy", 1, 0, c.getResources().getDrawable(R.drawable.przecier));
                 insertProdukt("zioła prowansalskie", 0, 0, c.getResources().getDrawable(R.drawable.ziola));
+                insertProdukt("czosnek", 2,0);
+                insertProdukt("śmietana", 1,0);
+                insertProdukt("mąka", 0,0);
+                insertProdukt("koperek", 0,0);
+                insertProdukt("natka pietruszki", 0,0);
+                insertProdukt("żeberko wołowe", 0,0);
+                insertProdukt("marchewka", 2,0);
+                insertProdukt("pietruszka", 2,0);
+                insertProdukt("seler", 2,0);
+                insertProdukt("majeranek", 0,0);
+                insertProdukt("ketchup", 1,0);
 
 
 
+                insertPrzepis("parówki z ketchupem", 10, "Wrzuć do gotującej się wody parówki. Gotuj pięć minut we wrzącej wodzie." +
+                        "Podawaj z chlebem i keczupem. Ewentualnie można też użyć musztardy.", 0, c.getResources().getDrawable(R.drawable.parowki_m_jez));
+                insertProduktPrzepis("parówki", "parówki z ketchupem", 4000, false);
+                insertProduktPrzepis("bułki", "parówki z ketchupem", 1000, true);
+                insertProduktPrzepis("ketchup","parówki z ketchupem", 2000, false);
 
-                insertPrzepis("Parówki z ketchupem", 10, "Wrzuć do gotującej się wody parówki. Gotuj pięć minut we wrzącej wodzie." + "Podawaj z chlebem i keczupem. Ewentualnie można też użyć musztardy.", 0);
-                insertProduktPrzepis("parówki", "Parówki z ketchupem", 4000, false);
-                insertProduktPrzepis("bułki", "Parówki z ketchupem", 1000, true);
-                insertProduktPrzepis("ketchup","Parówki z ketchupem", 2000, false);
+                insertPrzepis("kanapki ze szprotami", 10, "Chleb posmaruj masłem oraz połóż na nim starannie " +
+                        "rozgniecione widelcem szproty." + "Dla smaku możesz położyć na nich ser biały.", 4, c.getResources().getDrawable(R.drawable.kanapki_ze_szprot));
+                insertProduktPrzepis("szproty w pomidorach", "kanapki ze szprotami", 1000, false);
+                insertProduktPrzepis("chleb", "kanapki ze szprotami", 300, false);
+                insertProduktPrzepis("ser biały", "kanapki ze szprotami", 900, true);
 
-                insertPrzepis("Kartofle z kefirem", 30, "Ziemniaki obierz a następnie gotuj dopóki nie zmiękną. W przypadku młodych ziemniaków pogruchotaj je, jeśli są stare utłucz je na puree." +
-                        "Posól wedle uznania oraz zalej kefirem. Udekoruj koperkiem dla smaku.", 2);
-                insertProduktPrzepis("ziemniaki", "Kartofle z kefirem", 700, false);
-                insertProduktPrzepis("kefir", "Kartofle z kefirem", 350, false);
-                insertProduktPrzepis("koperek", "Kartofle z kefirem", 100, true);
+                insertPrzepis("kartofle z kefirem", 30, "Ziemniaki obierz a następnie gotuj dopóki nie zmiękną. " +
+                        "W przypadku młodych ziemniaków pogruchotaj je, jeśli są stare utłucz je na puree. " +
+                        "Posól wedle uznania oraz zalej kefirem. Udekoruj koperkiem dla smaku.", 2, c.getResources().getDrawable(R.drawable.kart_z_kef));
+                insertProduktPrzepis("ziemniaki", "kartofle z kefirem", 700, false);
+                insertProduktPrzepis("kefir", "kartofle z kefirem", 350, false);
+                insertProduktPrzepis("koperek", "kartofle z kefirem", 100, true);
 
-                insertPrzepis("Kanapki ze szprotami", 10, "Chleb posmaruj masłem oraz połóż na nim starannie " +
-                        "rozgniecione widelcem szproty." + "Dla smaku możesz położyć na nich ser biały.", 4);
-                insertProduktPrzepis("szproty w pomidorach", "Kanapki ze szprotami", 1000, false);
-                insertProduktPrzepis("chleb", "Kanapki ze szprotami", 300, false);
-                insertProduktPrzepis("ser biały", "Kartofle z kefirem", 900, true);
 
-                insertPrzepis("Spaghetti bolongese", 40, "Na patelni podgrzej olej i zeszlij cebulę oraz czosnek. Dodaj mięso mielone i podsmaż je. " + "Dodaj przecier pomidorowy i duś na wolnym ogniu." +
-                        "Jeżeli masz ochotę dodaj zioła prowansalskie." + "Makaron wrzuć do gotującej się wody i gotuj na wolnym ogniu około 8 minut mieszając od czasu do " + "Odcedź makaron i połóż go na talerzu wraz z sosem.", 2);
-                insertProduktPrzepis("mięso mielone", "Spaghetti bolongese", 200, false);
-                insertProduktPrzepis("cebula", "Spaghetti bolongese", 1000, false);
-                insertProduktPrzepis("czosnek", "Spaghetti bolongese", 400, false);
-                insertProduktPrzepis("przecier pomidorowy", "Spaghetti bolongese", 300, false);
-                insertProduktPrzepis("makaron", "Spaghetti bolongese", 250, false);
-                insertProduktPrzepis("zioła prowansalskie", "Spaghetti bolongese", 300, true);
+                insertPrzepis("spaghetti bolongese", 40, "Na patelni podgrzej olej i zeszlij cebulę oraz czosnek. Dodaj mięso mielone i podsmaż je. " + "Dodaj przecier pomidorowy i duś na wolnym ogniu." +
+                        "Jeżeli masz ochotę dodaj zioła prowansalskie." + "Makaron wrzuć do gotującej się wody i gotuj na wolnym ogniu około 8 minut mieszając od czasu do " +
+                        "Odcedź makaron i połóż go na talerzu wraz z sosem.", 2, c.getResources().getDrawable(R.drawable.spaghetti));
+                insertProduktPrzepis("mięso mielone", "spaghetti bolongese", 200, false);
+                insertProduktPrzepis("cebula", "spaghetti bolongese", 1000, false);
+                insertProduktPrzepis("czosnek", "spaghetti bolongese", 400, false);
+                insertProduktPrzepis("przecier pomidorowy", "spaghetti bolongese", 300, false);
+                insertProduktPrzepis("makaron", "spaghetti bolongese", 250, false);
+                insertProduktPrzepis("zioła prowansalskie", "spaghetti bolongese", 300, true);
 
-                insertPrzepis("Schab w sosie własnym", 60, "Schab pokroić w plastry o grubości 1-2 cm" +
+                insertPrzepis("schab w sosie własnym", 60, "Schab pokroić w plastry o grubości 1-2 cm" +
                         "Kawałki mięsa położyć na suchej dobrze nagrzanej patelni; Mięso powinno prawie,że się przypalić, nabrać koloru mocno brązowego z obydwu stron, to właśnie te przypalone soki nadadzą wyjątkowego smaku sosowi;Proszę się nie martwić suchością i przypalenizną, podczas duszenia mięso nabierze konsystencji takiej,że będzie się rozpływać w buzi.\n" +
                         "Gdy mięso jest dość dobrze przyrumienione, dodać posiekaną cebulkę i zalać wodą tak by przykrywało mięso, doprawić solą i pieprzem do smaku; Przykryć pokrywką i dusić na małym ogniu ok 40-60 minut. W razie potrzeby dolać wody (gdyby się wygotowała)\n" +
                         "Gdy mięso jest mięciutkie, zagęścić sos łyżką mąki wymieszaną z 50 ml zimnej wody; Następnie dodać łyżkę śmietany, posiekaną natkę pietruszki,zamięszać i wyłączyć zródło ciepła;\n" +
-                        "Sos i schab gotowy - smacznego!", 2);
-                insertProduktPrzepis("schab", "Schab w sosie własnym", 500, false);
-                insertProduktPrzepis("cebula", "Schab w sosie własnym", 1000, false);
-                insertProduktPrzepis("mąka", "Schab w sosie własnym", 50, false);
-                insertProduktPrzepis("śmietana", "Schab w sosie własnym", 50, false);
-                insertProduktPrzepis("natka pietruszki", "Schab w sosie własnym", 1000, false);
+                        "Sos i schab gotowy - smacznego!", 2, c.getResources().getDrawable(R.drawable.schab_w_sosie));
+                insertProduktPrzepis("schab", "schab w sosie własnym", 500, false);
+                insertProduktPrzepis("cebula", "schab w sosie własnym", 1000, false);
+                insertProduktPrzepis("mąka", "schab w sosie własnym", 50, false);
+                insertProduktPrzepis("śmietana", "schab w sosie własnym", 50, false);
+                insertProduktPrzepis("natka pietruszki", "schab w sosie własnym", 1000, false);
 
-                insertPrzepis("Zupa fasolowa", 40, "Dzień wcześniej namoczyć fasolę: wsypać do garnka, zalać zimną wodą w ilości około 1 litra i odstawić na całą noc do napęcznienia.\n" +
+                insertPrzepis("zupa fasolowa", 40, "Dzień wcześniej namoczyć fasolę: wsypać do garnka, zalać zimną wodą w ilości około 1 litra i odstawić na całą noc do napęcznienia.\n" +
                         "Następnego dnia odcedzić fasolę. Do dużego garnka włożyć pokrojone na 2 - 3 kawałki żeberka, wlać 2,5 litra wody, dodać fasolę oraz łyżeczkę soli. Zagotować, zszumować, zmniejszyć ogień i gotować pod przykryciem przez ok. 1 godzinę i 15 minut.\n" +
-                        "Dodać 1 obraną marchewkę (drugą obrać, zetrzeć na tarce o dużych oczkach i odłożyć na później), obraną pietruszkę, kawałek korzenia selera oraz ziela angielskie i listek laurowy. Gotować przez 15 minut.\n" +
-                        "Odkroić skórkę z boczku, wykroić białe chrząstki, pokroić w kosteczkę. Włożyć na patelnię i co chwilę mieszając podsmażyć na złoty kolor. Dodać pokrojoną w kosteczkę cebulę i mieszając smażyć przez ok. 5 minut. Pod koniec dodać rozgnieciony czosnek. Przełożyć do garnka z wywarem wypłukując patelnię częścią wywaru.\n" +
+                        "Dodać 1 obraną marchewkę (drugą obrać, zetrzeć na tarce o dużych oczkach i odłożyć na później), obraną pietruszkę oraz kawałek korzenia selera. Gotować przez 15 minut.\n" +
+                        "Włożyć na patelnię pokrojoną w kosteczkę cebulę i mieszając smażyć przez ok. 5 minut. Pod koniec dodać rozgnieciony czosnek. Przełożyć do garnka z wywarem wypłukując patelnię częścią wywaru.\n" +
                         "Do zupy dodać odłożoną startą marchewkę, doprawić świeżo zmielonym pieprzem i w razie potrzeby solą. Gotować 10 minut.\n" +
-                        "Ziemniaki obrać i pokroić w kosteczkę, dodać do zupy. Gotować przez 1/2 godziny. Na koniec dodać majeranek i chwilę pogotować. Podawać z pieczywem.", 2);
-                insertProduktPrzepis("ziemniaki", "Zupa fasolowa", 200, false);
-                insertProduktPrzepis("żeberko wołowe", "Zupa fasolowa", 500, false);
-                insertProduktPrzepis("marchewka", "Zupa fasolowa", 100, false);
-                insertProduktPrzepis("pietruszka", "Zupa fasolowa", 100, false);
-                insertProduktPrzepis("seler", "Zupa fasolowa", 100, false);
-                insertProduktPrzepis("ziemniaki", "Zupa fasolowa", 200, false);
-                insertProduktPrzepis("ziele angielskie", "Zupa fasolowa", 50, false);
-                insertProduktPrzepis("liść laurowy", "Zupa fasolowa", 1000, false);
-                insertProduktPrzepis("boczek", "Zupa fasolowa", 300, false);
-                insertProduktPrzepis("cebula", "Zupa fasolowa", 1000, false);
-                insertProduktPrzepis("czosnek", "Zupa fasolowa", 400, false);
-                insertProduktPrzepis("majeranek", "Zupa fasolowa", 20, false);
-                insertProduktPrzepis("fasola czarna", "Zupa fasolowa", 200, false);
-                insertProduktPrzepis("fasola biała", "Zupa fasolowa", 200, false);
+                        "Ziemniaki obrać i pokroić w kosteczkę, dodać do zupy. Gotować przez 1/2 godziny. Na koniec dodać majeranek i chwilę pogotować. Podawać z pieczywem.", 2, c.getResources().getDrawable(R.drawable.zupa_fasolowa));
+                insertProduktPrzepis("ziemniaki", "zupa fasolowa", 200, false);
+                insertProduktPrzepis("żeberko wołowe", "zupa fasolowa", 500, false);
+                insertProduktPrzepis("marchewka", "zupa fasolowa", 100, false);
+                insertProduktPrzepis("pietruszka", "zupa fasolowa", 100, false);
+                insertProduktPrzepis("seler", "zupa fasolowa", 100, false);
+                insertProduktPrzepis("ziemniaki", "zupa fasolowa", 200, false);
+                insertProduktPrzepis("cebula", "zupa fasolowa", 1000, false);
+                insertProduktPrzepis("czosnek", "zupa fasolowa", 400, false);
+                insertProduktPrzepis("majeranek", "zupa fasolowa", 20, false);
+                insertProduktPrzepis("fasola czarna", "zupa fasolowa", 200, false);
+                insertProduktPrzepis("fasola biała", "zupa fasolowa", 200, false);
 
             }
             return null;
